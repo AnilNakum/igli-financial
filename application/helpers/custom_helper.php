020569a03714905a803f9738a174814c2efd82ec
Anil Nakum <anilnakum.ibl@gmail.com>
2024-01-19 13:29:03 +0530

    API

--- application/helpers/custom_helper.php
+++ application/helpers/custom_helper.php
@@ -362,7 +362,7 @@ function does_url_exists($url)
 */
 function cca_encrypt($plainText,$key)
 {
-	$key = hextobin(md5($key));
+	$key = cca_hextobin(md5($key));
 	$initVector = pack("C*", 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f);
 	$openMode = openssl_encrypt($plainText, 'AES-128-CBC', $key, OPENSSL_RAW_DATA, $initVector);
 	$encryptedText = bin2hex($openMode);
@@ -376,9 +376,9 @@ function cca_encrypt($plainText,$key)
 */
 function cca_decrypt($encryptedText,$key)
 {
-	$key = hextobin(md5($key));
+	$key = cca_hextobin(md5($key));
 	$initVector = pack("C*", 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f);
-	$encryptedText = hextobin($encryptedText);
+	$encryptedText = cca_hextobin($encryptedText);
 	$decryptedText = openssl_decrypt($encryptedText, 'AES-128-CBC', $key, OPENSSL_RAW_DATA, $initVector);
 	return $decryptedText;
 }
